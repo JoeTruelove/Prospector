1428cbac00026bcea3ef5ab656ee50cbc3145f8c
Joseph Truelove <43154676+JoeTruelove@users.noreply.github.com>
2019-10-16 21:00:15 -0400

    Added Score System
    Added Score Manager script and changed Prospector script. Console now shows score system.

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -189,6 +189,7 @@ public class Prospector : MonoBehaviour {
                 MoveToDiscard(target);
                 MoveToTarget(Draw());
                 UpdateDrawPile();
+                ScoreManager.EVENT(eScoreEvent.draw);
                 break;
 
             case eCardState.tableau:
@@ -205,6 +206,7 @@ public class Prospector : MonoBehaviour {
                 tableau.Remove(cd);
                 MoveToTarget(cd);
                 SetTableauFaces();
+                ScoreManager.EVENT(eScoreEvent.mine);
                 break;
         }
         CheckForGameOver();
@@ -235,11 +237,13 @@ public class Prospector : MonoBehaviour {
     {
         if(won)
         {
-            print("Game Over. You Won! :)");
+            //print("Game Over. You Won! :)");
+            ScoreManager.EVENT(eScoreEvent.gameWin);
         }
         else
         {
-            print("Game Over. You Lost. :(");
+            //print("Game Over. You Lost. :(");
+            ScoreManager.EVENT(eScoreEvent.gameLoss);
         }
         SceneManager.LoadScene("__Prospector_Scene_0");
     }
